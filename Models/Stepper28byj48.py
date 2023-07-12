17dd66603d4368b0551f1b78bfe7194e65612090
Raúl Caro Pastorino <raul@fryntiz.dev>
2023-07-12 20:33:20 +0200

    Enhanced methods

--- Models/Stepper28byj48.py
+++ Models/Stepper28byj48.py
@@ -34,18 +34,28 @@ class Stepper28byj48:
 
 
 
-    def __init__(self, pinArray, direction = 'clockwise', speed = 'slow', debug = False):
+    def __init__(self, pinArray, direction = 'clockwise', speed = 'slow', motorSpeed = 1200, stepsPerRev = 4076, debug = False):
         """
         Inicializa el motor
         @param pinArray: Array con los pines del motor en orden, [IN1, IN2, IN3, IN4]
         @param direction: Dirección del motor, clockwise o counterclockwise
         @param speed: Velocidad del motor, slow, medium o hight
+        @param motorSpeed: Velocidad del motor en microsegundos, 1200 por defecto aunque me funciona mejor con 2400
+        @param stepsPerRev: Pasos por revolución, 4076 por defecto. Debería ser 4096 pero en estos motores se descuentan (leer datasheet)
         @param debug: Muestra mensajes de debug
         """
         self.debug = debug
+        self.active = False
+
+        if motorSpeed and motorSpeed > 1200:
+            self.motorSpeed = motorSpeed
+
+        if stepsPerRev > 100:
+            self.stepsPerRev = stepsPerRev
 
         if len(pinArray) != 4:
             raise Exception('Error: Debe ingresar 4 pines')
+
 
         self.pinArray = pinArray
 
@@ -67,37 +77,34 @@ class Stepper28byj48:
             print('Dirección: ', self.direction)
 
 
-    def start(self):
-        """
-        Inicia el motor
-        """
-        motorSpeed = 1200;
-        stepCounter = 0;
-        stepsPerRev = 4076;
+    def run(self):
+        self.active = True
 
-        if self.debug:
-            print('Iniciando motor')
+        motorSpeed = self.motorSpeed
+        stepsPerRev = self.stepsPerRev
+        stepCounter = 0;
 
         pins = self.pins # Instancia con los pines del motor
         speedSteps = self.speedSteps[self.speed] # Instancia con los pasos del motor para la velocidad seleccionada
-        steps = len(speedSteps); # 4 u 8 dependiendo del modo de velocidad
+        steps = len(speedSteps) # 4 u 8 dependiendo del modo de velocidad
 
-        #timer.init(freq=2.5, mode=Timer.PERIODIC, callback=blink)
-
-        while True:
+        while self.active:
 
             for i in range(0, (stepsPerRev * 2) - 1):
+
+                if not self.active:
+                    break
 
                 if stepCounter < (steps - 1):
                     stepCounter += 1
                 else:
                     stepCounter = 0
 
-                print('pins: ', pins)
-                print('pins[0]: ', pins[0])
-                print('pins[0].value: ', pins[0].value())
-                print('speedSteps', speedSteps)
-                print('speedSteps[stepCounter]', speedSteps[stepCounter])
+                #print('pins: ', pins)
+                #print('pins[0]: ', pins[0])
+                #print('pins[0].value: ', pins[0].value())
+                #print('speedSteps', speedSteps)
+                #print('speedSteps[stepCounter]', speedSteps[stepCounter])
 
 
                 pins[0].value(speedSteps[stepCounter][0])
@@ -108,20 +115,44 @@ class Stepper28byj48:
 
                 sleep(motorSpeed / 1000000.0) # Microsegundos a Segundos
 
+    def start(self):
+        """
+        Inicia el motor
+        """
+
+        if self.debug:
+            print('Iniciando motor')
+
+        self.run()
+        #timer.init(freq=2.5, mode=Timer.PERIODIC, callback=blink)
 
 
 
+    def stop(self):
+        """
+        Detiene el motor apagando todos los pines y marca el estado como inactivo.
+        """
+        self.active = False
+        sleep(0.1)
 
+        pins = self.pins
 
-
-
-
+        pins[0].off()
+        pins[1].off()
+        pins[2].off()
+        pins[3].off()
 
     def setDirection(self, direction):
+        if direction in ['clockwise', 'counterclockwise']:
+            self.direction = direction
+
         if self.debug:
             print('Dirección: ', direction)
 
 
     def setSpeed(self, speed):
+        if speed in ['slow', 'medium', 'hight']:
+            self.speed = speed
+
         if self.debug:
             print('Velocidad: ', speed)
